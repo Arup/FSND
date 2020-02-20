fe95e02fe9bb3e434077fdd8715cf1961f8d9710
Arup Kabi <arupkumarkabi@gmail.com>
2020-02-20 00:07:17 -0800

    popo

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import sqlalchemy.pool as pool
+import psycopg2
 
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
@@ -20,3 +22,17 @@ Base = declarative_base()
 SQLALCHEMY_DATABASE_URI = 'postgresql://postgresuser:@localhost:5432/test'
 engine = create_engine(SQLALCHEMY_DATABASE_URI)
 Base.metadata.create_all(engine)
+
+def getconn():
+    c = psycopg2.connect(user='postgresuser', host='localhost', dbname='test')
+    return c
+
+mypool = pool.QueuePool(getconn, max_overflow=10, pool_size=5)
+
+conn = mypool.connect()
+
+#print(conn.info())
+
+print(conn.__dict__)
+
+conn.close()
